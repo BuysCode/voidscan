dac73ffb22d96a12f2e9ab091eba4c2b4a734f0d
Guilherme Buys <guibuys55@gmail.com>
2026-04-28 22:23:04 -0300

    feat: analysing .env-like files has been ignored by git ignore

--- src/options/handlers.ts
+++ src/options/handlers.ts
@@ -11,16 +11,31 @@ export async function gitIgnoreCheck(projectPath: string) {
         return { success: false, message: "Arquivo .gitignore não encontrado." }
     }
 
-    const content = await gitIgnore.text()
-    if (content.trim().length === 0) {
+    const gitIgnoreContent = await gitIgnore.text()
+
+    if (gitIgnoreContent.trim().length === 0) {
         return { success: false, message: "O seu .gitignore está vazio." }
     }
 
     const envFile = file(pathJoin(projectPath, '.env'));
     const envExists = await envFile.exists();
 
-    if (envExists && !content.includes('.env')) {
+    const localEnv = file(pathJoin(projectPath, '.env.local'));
+    const localEnvExists = await localEnv.exists();
+
+    const testEnv = file(pathJoin(projectPath, '.env.test'));
+    const testEnvExists = await testEnv.exists();
+
+    if (envExists && !gitIgnoreContent.includes('.env')) {
         return { success: false, message: "Arquivo .env detectado mas não ignorado no .gitignore." }
+    }
+
+    if (localEnvExists && !gitIgnoreContent.includes('.env.local')) {
+        return { success: false, message: "Arquivo .env.local detectado mas não ignorado no .gitignore." }
+    }
+
+    if (testEnvExists && !gitIgnoreContent.includes(".env.test")) {
+        return { success: false, message: "Arquivo .env.test detectado mas não ignorado no .gitignore." }
     }
 
     return { success: true }
